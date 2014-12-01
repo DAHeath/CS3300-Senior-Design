ffc21b648b35275bcb754de1c33e9723f5c6eece
Mick Glover <mickglover1@gmail.com>
2014-12-01 16:00:00 -0500

    Added Record validation to students
    Added record Validation for the length of students first and last names

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,9 @@
 class Student < ActiveRecord::Base
   belongs_to :team
   has_many :join_team_contracts
-	
+  validates :first_name, length: {in: 3..20}
+  validates :last_name, length: {in: 3..20}
+
 	def full_name
     "#{first_name} #{last_name}"
   end
